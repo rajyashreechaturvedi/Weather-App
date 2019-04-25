8cde78e9609e80f9a24dcd3a282626fe00ab88f9
Rajyashree Chaturvedi <rajyashree.chaturvedi@nagarro.com>
2019-04-25 12:34:42 +0530

    silly mistake

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/weather-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(__dirname+'/dist/<name-of-app>/index.html');
+res.sendFile(__dirname+'/dist/weather-app/index.html');
 });
 
 // Start the app by listening on the default Heroku port
