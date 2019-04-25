53ebe53233cb739476098fb429b02b058acf53d6
Rajyashree Chaturvedi <rajyashree.chaturvedi@nagarro.com>
2019-04-25 12:27:50 +0530

    remove path dependency

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 //Install express server
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
@@ -9,7 +8,7 @@ app.use(express.static(__dirname + '/dist/<name-of-app>'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+res.sendFile(__dirname+'/dist/<name-of-app>/index.html');
 });
 
 // Start the app by listening on the default Heroku port
